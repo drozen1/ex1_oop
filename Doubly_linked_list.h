a93404094b21d8dc84e5fed6c8189bbb16f1bb74
drozen <drozen@campus.technion.ac.il>
2020-05-04 14:04:06 +0300

    push new function of list

--- Doubly_linked_list.h
+++ Doubly_linked_list.h
@@ -16,8 +16,96 @@ namespace AVL {
     private:
         Link_Node<T>* Head;
         Link_Node<T>* Last;
+    public:
+        Doubly_Linked_List();
 
+        Doubly_Linked_List(Link_Node<T> *head);
+
+        virtual ~Doubly_Linked_List();
+
+        Link_Node<T> *getHead() const;
+
+        void setHead(Link_Node<T> *head);
+
+        Link_Node<T> *getLast() const;
+
+        void setLast(Link_Node<T> *last);
+
+       void setNewNodeAfterNode(Link_Node<T> *push_after_node,Link_Node<T>* newNode);
     };
+
+    template<class T>
+    Doubly_Linked_List<T>::Doubly_Linked_List():Head(NULL),Last(NULL) {
+
+    }
+
+    template<class T>
+    Doubly_Linked_List<T>::Doubly_Linked_List(Link_Node<T> *head):Head(head) {}
+
+    template<class T>
+    Doubly_Linked_List<T>::~Doubly_Linked_List() {
+
+    }
+
+    template<class T>
+    Link_Node<T> *Doubly_Linked_List<T>::getHead() const {
+        return Head;
+    }
+
+    template<class T>
+    void Doubly_Linked_List<T>::setHead(Link_Node<T> *head) {
+        Head = head;
+    }
+
+    template<class T>
+    Link_Node<T> *Doubly_Linked_List<T>::getLast() const {
+        return Last;
+    }
+
+    template<class T>
+    void Doubly_Linked_List<T>::setLast(Link_Node<T> *last) {
+        Last = last;
+    }
+
+    /**
+     * in case node==NULL we want to push in the head
+     * @tparam T
+     * @param push_after_node
+     * @param newNode- the address of the new node..
+     */
+    template<class T>
+    void Doubly_Linked_List<T>::setNewNodeAfterNode(Link_Node<T> *push_after_node,Link_Node<T>* newNode) {
+        ///treat head is newNode
+        if (push_after_node==NULL){
+            if(this->getHead()!=NULL) {
+                newNode->setNext(this->getHead());
+                if(this->getHead()->getNext()!=NULL) {
+                    this->getHead()->getNext()->setPrev(newNode);
+                }
+            }
+            else{
+                setLast(newNode);
+            }
+            this->setHead(newNode);
+            return;
+
+        }
+        ///treat last
+        if (push_after_node->getNum()==getLast()->getNum()){
+            push_after_node->setNext(newNode);
+            setLast(newNode);
+            newNode->setPrev(push_after_node);
+            return;
+        }
+        ///treat middle case
+        Link_Node<T>* push_before_node=push_after_node->getNext();
+        push_after_node->setNext(newNode);
+        newNode->setPrev(push_after_node);
+        push_before_node->setPrev(newNode);
+        newNode->setNext(push_before_node);
+        return;
+
+    }
 
 }
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,52 @@
 #include <iostream>
 #include "AVL_tree.h"
+#include "Doubly_linked_list.h"
 
 int main() {
+
+    AVL::Doubly_Linked_List<int> list;
+    AVL::Link_Node<int> node1= AVL::Link_Node<int>(1);
+    AVL::Link_Node<int> node2= AVL::Link_Node<int>(2);
+    AVL::Link_Node<int> node3= AVL::Link_Node<int>(3);
+    AVL::Link_Node<int> node4= AVL::Link_Node<int>(4);
+    AVL::Link_Node<int> node5= AVL::Link_Node<int>(5);
+    AVL::Link_Node<int> node6= AVL::Link_Node<int>(6);
+    AVL::Link_Node<int> node7= AVL::Link_Node<int>(7);
+    AVL::Link_Node<int> node8= AVL::Link_Node<int>(8);
+
+    list.setNewNodeAfterNode(NULL,&node1);
+    list.setNewNodeAfterNode(&node1,&node2);
+    list.setNewNodeAfterNode(&node2,&node3);
+    list.setNewNodeAfterNode(&node2,&node4);
+    list.setNewNodeAfterNode(NULL,&node5);
+    list.setNewNodeAfterNode(&node3,&node6);
+
+
+
+    AVL::Link_Node<int>* ptr=list.getHead();
+    int i=1;
+    while(ptr!=NULL){
+        std::cout<<"Node number "<<i<< " is: "<<ptr->getNum()<<std::endl;
+        ptr=ptr->getNext();
+        i++;
+    }
+    std::cout<<"The Head is: "<<list.getHead()->getNum()<<std::endl;
+    std::cout<<"The Last is: "<<list.getLast()->getNum()<<std::endl;
+
+
+
+
+
+
+
+
+
+
 
 AVL::AVL_tree_node<int> node_temp5 =   AVL::AVL_tree_node<int>(2,5);
 AVL::AVL_tree_node<int> node_temp2 =   AVL::AVL_tree_node<int>(2,2);
 AVL::AVL_tree_node<int> node_temp8 =   AVL::AVL_tree_node<int>(2,8);
 AVL::AVL_tree_node<int> node_temp1 =   AVL::AVL_tree_node<int>(2,1);
-
 AVL::AVL_tree_node<int> node_temp4 =   AVL::AVL_tree_node<int>(2,4);
 AVL::AVL_tree_node<int> node_temp6 =   AVL::AVL_tree_node<int>(2,6);
 AVL::AVL_tree_node<int> node_temp10 =   AVL::AVL_tree_node<int>(2,10);
@@ -16,10 +55,6 @@ AVL::AVL_tree_node<int> node_temp7 =   AVL::AVL_tree_node<int>(2,7);
 AVL::AVL_tree_node<int> node_temp9 =   AVL::AVL_tree_node<int>(2,9);
 AVL::AVL_tree_node<int> node_temp12 =   AVL::AVL_tree_node<int>(2,12);
 AVL::AVL_tree_node<int> node_temp11 =   AVL::AVL_tree_node<int>(2,11);
-
-
-
-
 
 
 AVL::AVL_tree<int> check;;
@@ -37,14 +72,14 @@ check.insert(node_temp12);
 check.insert(node_temp11);
 
 
-check.remove(node_temp1);
+//check.remove(node_temp1);
 //StatusType AVL_tree<Element>::remove(AVL::AVL_tree<Element> &node_to_remove)
     //    StatusType AVL_tree<Element>::insert(AVL_tree_node<Element> &node_toadd) {
 
    ////////////////////////////////////////////////////////neet to update height in the remove function at the way back
 
 //    AVL_tree_node<int>* node_temp3=check.find_node(8);
-    check.inOrder(check.getRoot());
+ //   check.inOrder(check.getRoot());
   //std::cout << "node_temp10.calcBalnceFactor() "<< std::endl;
     //node_temp1=check.find_node(7);
 
@@ -55,7 +90,7 @@ check.remove(node_temp1);
 
 //    AVL_tree_node<int>* curr = &node_temp;
 //    if(curr!=NULL && (curr->getLeftSon() != NULL or curr->getRightSon() != nullptr)){
-        std::cout << "the root is: " << check.getRoot()->getKey() << std::endl;
+//        std::cout << "the root is: " << check.getRoot()->getKey() << std::endl;
 //    }
 
     return 0;
